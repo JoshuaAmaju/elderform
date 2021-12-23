d087415f2d91ced799a63849ca234ada70ef3b4f
Joshua Amaju <princejosh0007@gmail.com>
2021-12-23 13:09:50 +0100

    add initial config types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,9 @@
-import { ZodTypeAny, string, object } from 'zod';
+import { ZodRawShape, ZodType } from 'zod';
 
-export type Validator = ZodTypeAny;
+export type Schema<T extends ZodRawShape> = { [K in keyof T]: ZodType<T[K]> };
+
+export type Config<T extends ZodRawShape, D = any> = {
+  schema: Schema<T>;
+  onSubmit: (value: T) => Promise<D>;
+  initialValues: { [K in keyof T]: T[K] };
+};
