22b056dfb9a77db5db64ee4a5ab9398445855eb5
Joshua Amaju <princejosh0007@gmail.com>
2021-12-25 13:37:08 +0100

    fix(tsconfig): github action build

--- src/machine/index.ts
+++ src/machine/index.ts
@@ -1,4 +1,4 @@
-import { pipe } from 'fp-ts/function';
+import { pipe } from 'fp-ts/lib/function';
 import * as O from 'fp-ts/Option';
 import { identity, keys, length, map } from 'ramda';
 import { actions, ActorRef, assign, createMachine, send, spawn } from 'xstate';
