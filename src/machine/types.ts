a82755fc01bf364f08d715ddb7356e4b327f77e0
Joshua Amaju <princejosh0007@gmail.com>
2022-01-08 20:22:55 +0100

    temp

--- src/machine/types.ts
+++ src/machine/types.ts
@@ -1,4 +1,63 @@
-export type Schema<T = any> = { [K in keyof T]: Validator<T, T[K]> };
+export type Schema<T = any> = {
+  [K in keyof T]: Schema<T[K]> | Validator<T, T[K]>;
+};
+
+export type FlattenedSchema<T extends Schema = any> = {
+  [K in keyof T]: T[K] extends Schema<infer R> ? Schema<R> : T[K];
+};
+
+type Dotify<A, B> = `${string & A}.${string & B}`;
+
+export type RecursiveKeyOf<T extends object> = {
+  [K in keyof T & (string | number)]: RecursiveKeyOfHandleValue<T[K], `${K}`>;
+}[keyof T & (string | number)];
+
+type RecursiveKeyOfInner<T extends object> = {
+  [K in keyof T & (string | number)]: RecursiveKeyOfHandleValue<T[K], `.${K}`>;
+}[keyof T & (string | number)];
+
+type RecursiveKeyOfHandleValue<
+  TValue,
+  Text extends string
+> = TValue extends any[]
+  ? Text
+  : TValue extends object
+  ? Text | `${Text}${RecursiveKeyOfInner<TValue>}`
+  : Text;
+
+type N = {
+  name: string;
+  company: {
+    address: {
+      age: string;
+    };
+  };
+  age: string;
+};
+
+const n = {
+  name: () => '',
+  company: {
+    address: {
+      age: () => '',
+    },
+  },
+  age: () => '',
+};
+
+type M = Infer<typeof n>;
+
+type O<T extends object> = {
+  [K in
+    | keyof T
+    | Dotify<keyof T, { [P in keyof T[keyof T]]: any }>]: K extends keyof T
+    ? T[K]
+    : T[keyof T];
+};
+
+type Y = O<typeof n>;
+
+// let u: Y = ['name'];
 
 export type Validator<SchemaType = any, T = any> = (
   value: T,
---
+++ src/machine/utils.ts
@@ -0,0 +1,5 @@
+import { Schema } from './types';
+
+export const flatten = <T extends Schema>(schema: T) => {
+  let _schema = {} as T;
+};
